e21f1903cb4705d2c3eb29457a5bc2feb61baebd
juanes <juanes210403@gmail.com>
2024-06-25 13:00:01 -0500

    proyecto crud

---
+++ src/test/java/com/crud/Juanes/JuanesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.juanes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantiagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
